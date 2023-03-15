3d0aeece84ddda999eca73e0ac9f86550b33f145
Sunny Batabyal <batabyalsunny@gmail.com>
2023-03-15 20:25:59 +0530

    Directory created using python script

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ElectronService } from 'ngx-electron';
 const fs = window.require('fs');
+const {PythonShell} = window.require('python-shell');
 
 @Component({
   selector: 'app-root',
@@ -12,12 +13,27 @@ export class AppComponent {
 
   constructor(private _electronService: ElectronService) { }
 
-  public createDirectory() {
-    let dir = "/Users/sunny/workspace/HelloWorld";
+  public createDirectoryUsingElectron() {
+    let dir = "/Users/sunny/workspace/my-electron-directory";
     if (!fs.existsSync(dir)) {
       fs.mkdirSync(dir, {
         recursive: true
       });
     }
+  }
+
+  public createDirectoryUsingPythonScript() {
+    let pyshell = new PythonShell('/Users/sunny/workspace/electron/electron-angular-app/test.py');
+
+    pyshell.on('message', function(message:any) {
+        console.log(message);
+    })
+
+    pyshell.end(function (err:any) {
+        if (err){
+            throw err;
+        };
+        console.log('finished');
+    });
   }
 }
